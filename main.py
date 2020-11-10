38b6aad58c2c838eba919f36376be14e2ff146f6
Sven Kroell <sven.kroell@gmail.com>
2020-11-10 21:04:59 +0100

    first python init

---
+++ main.py
@@ -0,0 +1,27 @@
+import discord
+import os
+
+TOKEN = os.environ.get("BOT_TOKEN")
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await message.channel.send(msg)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+if __name__ == '__main__':
+    client.run(TOKEN)
+
